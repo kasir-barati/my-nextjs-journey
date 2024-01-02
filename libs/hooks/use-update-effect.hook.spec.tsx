bbbd1fac8b189ad031440d4876c897604002e1fc
Kasir Barati <kasir.barati@gmail.com>
2024-01-02 06:22:37 +0100

    chore: wip -- make test a little better

--- libs/hooks/use-update-effect.hook.spec.tsx
+++ libs/hooks/use-update-effect.hook.spec.tsx
@@ -3,17 +3,18 @@ import { useUpdateEffect } from './use-update-effect.hook';
 
 describe('useUpdateEffect', () => {
     it('should not call the callback on the first render, but should on subsequent renders', () => {
-        let fakeDependency = 1;
         const effectCallback = jest.fn();
 
-        const { rerender } = renderHook(() =>
-            useUpdateEffect(effectCallback, [fakeDependency]),
+        const { rerender } = renderHook(
+            ({ fakeDependency }: { fakeDependency: number }) =>
+                useUpdateEffect(effectCallback, [fakeDependency]),
+            { initialProps: { fakeDependency: 0 } },
         );
 
         // The first render skips the callback.
         expect(effectCallback).not.toHaveBeenCalled();
         // The second render invokes the callback.
-        act(() => fakeDependency++ && rerender());
+        act(() => rerender({ fakeDependency: 1 }));
         expect(effectCallback).toHaveBeenCalledTimes(1);
     });
 });
